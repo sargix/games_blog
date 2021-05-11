3f153d30b31cd53f2c2c18700a5fc04067139803
Damian Sarga <sarga.damian@gmail.com>
2021-05-11 12:40:35 +0200

    First app commit

---
+++ resources/views/blog/list.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main')
+
+@section('content')
+    <h1>lista</h1>
+@endsection
---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Games World</title>
+
+    @include('additives.styles')
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+    
+
+    @include('additives.scripts')
+</body>
+</html>
